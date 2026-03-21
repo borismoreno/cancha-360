875e269526f969eaacfd6d3f2728b0604f174074
Boris Moreno <boris_marco_moreno@hotmail.com>
2026-03-20 23:02:40 -0500

    mvp funcional

---
+++ packages/database/prisma/migrations/20260318171751_inviterole/migration.sql
@@ -0,0 +1,12 @@
+/*
+  Warnings:
+
+  - Changed the type of `role` on the `invitations` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
+
+*/
+-- AlterTable
+ALTER TABLE "invitations" DROP COLUMN "role",
+ADD COLUMN     "role" "Role" NOT NULL;
+
+-- DropEnum
+DROP TYPE "InvitationRole";
