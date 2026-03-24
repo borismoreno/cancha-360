928940672f70175cee6dbd0643b5f9f84bc9849e
Boris Moreno <boris_marco_moreno@hotmail.com>
2026-03-23 23:43:04 -0500

    set global prefix

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -6,6 +6,7 @@ import { AppModule } from './modules/app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.setGlobalPrefix('api');
   app.enableCors({
     origin: ['http://localhost:5173'],
     credentials: true,
